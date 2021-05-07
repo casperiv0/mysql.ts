6200a74a526ea6474724b7735ad0e37c29793af0
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-05-07 12:26:21 +0200

    :bug: fix: Add missing exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## 0.0.4
+
+- Add missing exports
+
+## 0.0.3
+
+- Add missing links to package.json
+
 ## 0.0.2
 
 - Update documentation
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Connection } from "./Connection";
 import { ConnectionConfig } from "./types";
+import { QueryBuilder } from "./QueryBuilder";
 
 export async function createConnection(config: ConnectionConfig): Promise<Connection> {
   return new Connection(config);
 }
+
+export { Connection, QueryBuilder };
+export type { ConnectionConfig };
